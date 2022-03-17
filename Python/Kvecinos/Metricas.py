e4ca3b6e6394aeabb44ec14e3f30ce69d9454229
Juan Sebastian Quintero <strangelife28@gmail.com>
2022-03-16 21:26:12 -0500

    Inyeccion de dependencias

--- Python/Kvecinos/Metricas.py
+++ Python/Kvecinos/Metricas.py
@@ -1,11 +1,10 @@
 
-from itertools import product
 from math import sqrt
 from KVecinos import Persona
 
-class Metricas(Persona):
-    def __init__(self):
-        Persona.__init__(self, " ", [])
+class Metricas():
+    def __init__(self, persona : Persona):
+        self.persona = persona
 
     def distanciaMetros(self, vector1, vector2):
         suma = 0 
@@ -24,10 +23,13 @@ class Metricas(Persona):
         suma = 0
         for indice in range(len(vector1)):
             suma += vector1[indice] * vector2[indice]
-            self.setValoraciones(vector1)
+            Persona.setValoraciones(vector1)
+            #self.setValoraciones(vector1)
             producto = self.calcularSumatoria()
-            self.setValoraciones(vector2)
-            producto *= self.calcularSumatoria()
+            #self.setValoraciones(vector2)
+            Persona.setValoraciones(vector2)
+            #producto *= self.calcularSumatoria()
+            producto *= Persona.calcularSumatoria()
             producto = producto / len(vector1)
 
             for i in range(len(vector1)):
@@ -36,12 +38,15 @@ class Metricas(Persona):
             numerador = suma + producto
 
             #Parte del denominador
-            self.setValoraciones(vector1)
-            termino1 = self.calcularSumaCuadrados() - (self.calcularCuadradoSuma() / len(vector1))
+            #self.setValoraciones(vector1)
+            Persona.setValoraciones(vector1)
+            #termino1 = self.calcularSumaCuadrados() - (self.calcularCuadradoSuma() / len(vector1))
+            termino1 = Persona.calcularSumaCuadrados() - (Persona.calcularCuadradoSuma() / len(vector1))
             termino1 = sqrt(termino1)
 
             self.setValoraciones(vector2)
-            termino2 = self.calcularSumaCuadrados() - (self.calcularCuadradoSuma() / len(vector2))
+            #termino2 = self.calcularSumaCuadrados() - (self.calcularCuadradoSuma() / len(vector2))
+            termino2 = Persona.calcularSumaCuadrados() - (Persona.calcularCuadradoSuma() / len(vector2))
             termino2 = sqrt(termino2)
 
             denominador = termino1 * termino2
@@ -49,7 +54,7 @@ class Metricas(Persona):
         
 
 
-metrica = Metricas()
+metrica = Metricas(Persona)#.distanciaMetros([1,2,3,4],[1,2,3,5])
 distancia = metrica.distanciaMetros([1,2,3,4],[1,2,3,5])
 print("Las distancia metro es:" + str(distancia))
 
